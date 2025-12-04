27aaa835ed22f39bd01955fe7a9bf33026976b72
AxPaw <ktoe.0426@icloud.com>
2025-12-04 17:30:06 +0900

    修正２

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import { Todo } from './Todo';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
